ea8299654dfc2bdb9402887bdf465c8602b3221a
Calvin Dalenta <calvindalentark@gmail.com>
2021-11-08 17:46:59 +0700

    Style ContentContainer

--- src/views/styled/ContentContainer.js
+++ src/views/styled/ContentContainer.js
@@ -5,6 +5,11 @@ export const ContentContainer = styled.main`
     min-height: 100vh;
     background-color: ${({theme}) => theme.background};
     padding: 4em 15em 0 4em;
+
+    @media screen and (max-width: 1080px){
+        width: 100%;
+        padding: 4em;
+    }
 `; 
 
 export default ContentContainer;
